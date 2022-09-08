7b7bcc3241a3dd95176cb1862bc3500df01df11b
Gerd Reiss <gerdreiss@@users.noreply.github.com>
2022-09-08 10:51:42 +0200

    [Rock the JVM Course Spark Streaming] Streaming Aggregations

--- src/main/scala/part2structuredstreaming/StreamingAggregations.scala
+++ src/main/scala/part2structuredstreaming/StreamingAggregations.scala
@@ -1,73 +1,66 @@
 package part2structuredstreaming
 
-import org.apache.spark.sql.{Column, DataFrame, SparkSession}
+import org.apache.spark.sql.{ Column, DataFrame, SparkSession }
 import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
 
 object StreamingAggregations {
 
-  val spark = SparkSession.builder()
+  val spark = SparkSession
+    .builder()
     .appName("Streaming Aggregations")
     .master("local[2]")
     .getOrCreate()
 
-  def streamingCount() = {
-    val lines: DataFrame = spark.readStream
+  def streamingCount() =
+    spark.readStream
       .format("socket")
       .option("host", "localhost")
       .option("port", 12345)
       .load()
-
-    val lineCount: DataFrame = lines.selectExpr("count(*) as lineCount")
-
-    // aggregations with distinct are not supported
-    // otherwise Spark will need to keep track of EVERYTHING
-
-    lineCount.writeStream
+      // aggregations with distinct are not supported
+      // otherwise Spark will need to keep track of EVERYTHING
+      .selectExpr("count(*) as lineCount")
+      .writeStream
       .format("console")
       .outputMode("complete") // append and update not supported on aggregations without watermark
       .start()
       .awaitTermination()
-  }
 
-  def numericalAggregations(aggFunction: Column => Column): Unit = {
-    val lines: DataFrame = spark.readStream
+  def numericalAggregations(aggFunction: Column => Column): Unit =
+    spark.readStream
       .format("socket")
       .option("host", "localhost")
       .option("port", 12345)
       .load()
-
-    // aggregate here
-    val numbers = lines.select(col("value").cast("integer").as("number"))
-    val aggregationDF = numbers.select(aggFunction(col("number")).as("agg_so_far"))
-
-    aggregationDF.writeStream
+      // aggregate here
+      .select(col("value").cast(IntegerType).as("number"))
+      .select(aggFunction(col("number")).as("agg_so_far"))
+      .writeStream
       .format("console")
       .outputMode("complete")
       .start()
       .awaitTermination()
-  }
 
-  def groupNames(): Unit = {
-    val lines: DataFrame = spark.readStream
+  def groupNames(): Unit =
+    spark.readStream
       .format("socket")
       .option("host", "localhost")
       .option("port", 12345)
       .load()
-
-    // counting occurrences of the "name" value
-    val names = lines
+      // counting occurrences of the "name" value
       .select(col("value").as("name"))
       .groupBy(col("name")) // RelationalGroupedDataset
       .count()
-
-    names.writeStream
+      .writeStream
       .format("console")
       .outputMode("complete")
       .start()
       .awaitTermination()
-  }
 
-  def main(args: Array[String]): Unit = {
-    groupNames()
-  }
+  def main(args: Array[String]): Unit =
+    // streamingCount()
+    // groupNames()
+    numericalAggregations(sum)
+
 }
