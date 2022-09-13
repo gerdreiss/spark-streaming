bffdf3ab34d35c1d921513a3e2c36b704fc0132e
Gerd Reiss <gerdreiss@@users.noreply.github.com>
2022-09-13 15:43:10 +0200

    [Rock The JVM Course Spark Streaming] Integration with Kafka

--- src/main/scala/part4integrations/IntegratingKafka.scala
+++ src/main/scala/part4integrations/IntegratingKafka.scala
@@ -1,73 +1,65 @@
 package part4integrations
 
-import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.{ DataFrame, SparkSession }
 import org.apache.spark.sql.functions._
 import common._
 
 object IntegratingKafka {
 
-  val spark = SparkSession.builder()
+  val spark = SparkSession
+    .builder()
     .appName("Integrating Kafka")
     .master("local[2]")
     .getOrCreate()
 
-  def readFromKafka() = {
+  def readFromKafka() =
     // https://spark.apache.org/docs/latest/structured-streaming-kafka-integration.html
-    val kafkaDF: DataFrame = spark.readStream
+    spark.readStream
       .format("kafka")
       .option("kafka.bootstrap.servers", "localhost:9092")
       .option("subscribe", "rockthejvm")
       .load()
-
-    kafkaDF
       .select(col("topic"), expr("cast(value as string) as actualValue"))
       .writeStream
       .format("console")
       .outputMode("append")
       .start()
       .awaitTermination()
-  }
 
-  def writeToKafka() = {
-    val carsDF = spark.readStream
+  def writeToKafka() =
+    spark.readStream
       .schema(carsSchema)
       .json("src/main/resources/data/cars")
-
-    val carsKafkaDF = carsDF.selectExpr("upper(Name) as key", "Name as value")
-
-    carsKafkaDF.writeStream
+      .selectExpr("upper(Name) as key", "Name as value")
+      .writeStream
       .format("kafka")
       .option("kafka.bootstrap.servers", "localhost:9092")
       .option("topic", "rockthejvm")
       .option("checkpointLocation", "checkpoints") // without checkpoints the writing to Kafka will fail
       .start()
       .awaitTermination()
-  }
 
   /**
     * Exercise: write the whole cars data structures to Kafka as JSON.
     * Use struct columns an the to_json function.
     */
-  def writeCarsToKafka() = {
-    val carsDF = spark.readStream
+  def writeCarsToKafka() =
+    spark.readStream
       .schema(carsSchema)
       .json("src/main/resources/data/cars")
-
-    val carsJsonKafkaDF = carsDF.select(
-      col("Name").as("key"),
-      to_json(struct(col("Name"), col("Horsepower"), col("Origin"))).cast("String").as("value")
-    )
-
-    carsJsonKafkaDF.writeStream
+      .select(
+        col("Name").as("key"),
+        to_json(struct(col("Name"), col("Horsepower"), col("Origin"))).cast("String").as("value"),
+      )
+      .writeStream
       .format("kafka")
       .option("kafka.bootstrap.servers", "localhost:9092")
       .option("topic", "rockthejvm")
       .option("checkpointLocation", "checkpoints")
       .start()
       .awaitTermination()
-  }
 
-  def main(args: Array[String]): Unit = {
+  def main(args: Array[String]): Unit =
     writeCarsToKafka()
-  }
+
 }
